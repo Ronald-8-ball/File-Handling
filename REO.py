b77f6534f3423c8a81af4fd5df2696dc56b53c3d
Ronald-8-ball <ronaldokuk462@gmail.com>
2024-12-09 19:03:47 +0100

    Initial

--- REO.py
+++ REO.py
@@ -8,3 +8,13 @@ file.close()
 
 file = open('File.txt','a')
 print(file.write('\nTo the next line'))
+
+file = open('File.txt','a+')
+file2 = open('App.txt','r')
+file.write(file2.read())
+
+file.seek(0)
+file2.seek(0)
+
+file.close()
+file2.close()
